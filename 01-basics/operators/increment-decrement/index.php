a87aded8839d068f3c694a987169c2872cce5abf
VishalGhuge111 <vishalghugesit@gmail.com>
2026-04-11 18:57:22 +0530

    implement increment/decrement operators

---
+++ 01-basics/operators/increment-decrement/index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Increment Decrement</title>
+</head>
+<body>
+<?php
+
+// increment and decrement
+
+$num1 = 5;
+echo "<p>num1: $num1</p>";
+$num1++;
+echo "<p>num1 after num1++: $num1</p>";
+
+$num2 = 5;
+echo "<p>num2: $num2</p>";
+$num2--;
+echo "<p>num2 after num2--: $num2</p>";
+
+$num3 = 5;
+echo "<p>num3: $num3</p>";
++$num3;
+echo "<p>num3 after ++num3: $num3</p>";
+
+$num4 = 5;
+echo "<p>num4: $num4</p>";
+--$num4;
+echo "<p>num4 after --num4: $num4</p>";
