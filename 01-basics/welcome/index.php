228342a3a6ac687bb83f69327098a8deb91dfbf2
VishalGhuge111 <vishalghugesit@gmail.com>
2026-04-11 18:25:00 +0530

    add welcome program and basic syntax

---
+++ 01-basics/welcome/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Welcome</title>
+</head>
+<body>
+
+<?php
+
+
+echo "<h1>Hello World</h1>";
+echo "<p>Welcome to PHP basics</p>";
+
+?>
+
+</body>
+</html>
