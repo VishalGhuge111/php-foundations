a427256390b8fbd143df78fefbaf5714631049b0
VishalGhuge111 <vishalghugesit@gmail.com>
2026-04-11 18:46:56 +0530

    practice comparison operators with var_dumps

---
+++ 01-basics/operators/comparison/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comparison Operators</title>
+</head>
+<body>
+<?php
+
+$a = 7;
+$b = 5;
+
+echo "<p>The value of a == b is:</p>";
+var_dump($a == $b);
+
+echo "<p>The value of a != b is:</p>";
+var_dump($a != $b);
+
+echo "<p>The value of a > b is:</p>";
+var_dump($a > $b);
+
+echo "<p>The value of a < b is:</p>";
+var_dump($a < $b);
+
+?>
+</body>
+</html>
